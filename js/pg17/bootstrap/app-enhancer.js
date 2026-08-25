9922005f66c39270e082f12170c7c98509bf2c62
didier2a <90510512+didier2a@users.noreply.github.com>
2026-08-25 19:01:58 +0200

    fix(pg17): retain guidance text after legacy renders

--- js/pg17/bootstrap/app-enhancer.js
+++ js/pg17/bootstrap/app-enhancer.js
@@ -83,7 +83,7 @@ proactiveEngine.options.poiRadiusMeters=-1;proactiveEngine.options.gpsAccuracyBa
 proactiveEngine.start();
 eventBus.on('voice.speaking',()=>{if(lastGuidanceText)setText('#guideAnswer',lastGuidanceText);});
 eventBus.on('voice.idle',()=>{if(lastGuidanceText)setText('#guideAnswer',lastGuidanceText);});
-eventBus.on('*',()=>{if(!pocketGuideState.select('ui.ar'))setText('#modeBadge','GUIDE AUDIOVISUEL');});
+eventBus.on('*',()=>{if(!pocketGuideState.select('ui.ar'))setText('#modeBadge','GUIDE AUDIOVISUEL');if(lastGuidanceText)setText('#guideAnswer',lastGuidanceText);});
 
 $('#startGuide')?.addEventListener('click',()=>setVoiceEnabled(true));
 $('#pg17VoiceToggle')?.addEventListener('click',()=>setVoiceEnabled(!voiceEnabled));
